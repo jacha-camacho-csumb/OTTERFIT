d6c85fe2e1f1557e1b0fe400dead90bf7cc864bc
nano-rta <nanortaanwar22@gmail.com>
2026-04-14 18:57:55 -0700

    Added workout controller, skeleton code.

---
+++ src/main/java/ui/workout/WorkoutController.java
@@ -0,0 +1,15 @@
+package ui.workout;
+
+import javafx.fxml.FXML;
+import javafc.scene.control.Alert;
+
+/**
+ * Handles user actions for logging and deleting workouts by displaying alerts.
+ *
+ * @author Nanorta Amwar
+ * @version 0.1.0
+ * @since 4/14/2026
+ */
+
+public class WorkoutController {
+}

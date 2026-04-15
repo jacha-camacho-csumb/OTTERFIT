71a8e6ebbf52696964933ac39f589593e88049fd
nano-rta <nanortaanwar22@gmail.com>
2026-04-14 19:43:49 -0700

    workout controller test skeleton

---
+++ src/test/java/WorkoutControllerTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import ui.workout.WorkoutController;
+
+/**
+ * Tests basic functionality of the WorkoutController class
+ *
+ * @author Nanorta Amwar
+ * @version 0.1.0
+ * @since 4/14/2026
+ */
+
+
+public class WorkoutControllerTest {
+}

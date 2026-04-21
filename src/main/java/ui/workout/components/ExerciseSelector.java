ff5aed537e3ce0f579624949f8654408b14d6e70
nano-rta <nanortaanwar22@gmail.com>
2026-04-21 16:39:28 -0700

    Add ExerciseSelector component skeleton

---
+++ src/main/java/ui/workout/components/ExerciseSelector.java
@@ -0,0 +1,24 @@
+/**
+ *
+ *
+ * @author Nanorta Amwar
+ * @version 0.1.0
+ * @since 4/21/2026
+ */
+
+package ui.workout.components;
+
+import db.Database.Exercise;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+/**
+ * Provides a reusable dropdown component for selecting an exercise.
+ * Uses data binding with ObservableList
+ */
+public class ExerciseSelector extends VBox {
+
+}

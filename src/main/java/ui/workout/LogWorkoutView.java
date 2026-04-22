553432e95fa4a40225e990633eafc98f3bca5b0e
nano-rta <nanortaanwar22@gmail.com>
2026-04-21 17:47:06 -0700

    Add LogWorkoutView basic structure

---
+++ src/main/java/ui/workout/LogWorkoutView.java
@@ -0,0 +1,18 @@
+package ui.workout;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Log Workout scene.
+ * Allows user to select an exercise, enter date, duration, notes, and save
+ * After saving, the form is cleared so another workout can be logged.
+ *
+ * @author Nanorta Amwar
+ * @version 0.1.0
+ * @since 4/21/2026
+ */
+
+
+public class LogWorkoutView {
+}

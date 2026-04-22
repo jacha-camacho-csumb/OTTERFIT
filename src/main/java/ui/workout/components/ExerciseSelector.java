a3f9c121fc5a3c174d8dcc66c6388fcf7a59ad62
nano-rta <nanortaanwar22@gmail.com>
2026-04-21 17:17:04 -0700

    jk this is the final final ExerciseSelector

--- src/main/java/ui/workout/components/ExerciseSelector.java
+++ src/main/java/ui/workout/components/ExerciseSelector.java
@@ -1,5 +1,12 @@
 package ui.workout.components;
 
+import db.Database.Exercise;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
 /**
  * Provides a reusable dropdown component for selecting an exercise.
  * Uses data binding with ObservableList
@@ -8,13 +15,6 @@ package ui.workout.components;
  * @version 0.1.0
  * @since 4/21/2026
  */
-
-import db.Database.Exercise;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
 
 // initialize layout and UI components
 public class ExerciseSelector extends VBox {
